a343fa9ceb16c8643f3e221cc50e62e994557b38
Marovlo <352907423@qq.com>
2023-04-13 11:00:18 +0000

    1

--- tensorTools.py
+++ tensorTools.py
@@ -7,13 +7,11 @@ class TENSORTOOLS():
 
     def tensor_to_byte(self,tensor:Tensor=not None):
         buf=io.BytesIO
-        buf.flush()
         torch.save(obj=tensor,f=buf)
         return buf.getvalue()
 
     def byte_to_tensor(self,data:bytes=not None):
         buf = io.BytesIO
-        buf.flush()
         buf.write(data)
         buf.seek(0)
         #print(buf.getvalue())
