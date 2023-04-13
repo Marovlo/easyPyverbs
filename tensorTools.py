298a2a08599c1f3daa1205003e765ed917143337
Marovlo <352907423@qq.com>
2023-04-13 10:58:46 +0000

    1

--- tensorTools.py
+++ tensorTools.py
@@ -4,18 +4,17 @@ import io
 
 
 class TENSORTOOLS():
-    # 工具类，用于tensor和byte在内存里（不经过文件系统）的相互转换
-    def __init__(self):
-        self.buf=io.BytesIO()
 
     def tensor_to_byte(self,tensor:Tensor=not None):
-        self.buf.flush()
-        torch.save(obj=tensor,f=self.buf)
-        return self.buf.getvalue()
+        buf=io.BytesIO
+        buf.flush()
+        torch.save(obj=tensor,f=buf)
+        return buf.getvalue()
 
     def byte_to_tensor(self,data:bytes=not None):
-        self.buf.flush()
-        self.buf.write(data)
-        self.buf.seek(0)
-        #print(self.buf.getvalue())
-        return torch.load(self.buf)
+        buf = io.BytesIO
+        buf.flush()
+        buf.write(data)
+        buf.seek(0)
+        #print(buf.getvalue())
+        return torch.load(buf)
