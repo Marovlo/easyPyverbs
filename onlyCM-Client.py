217828bb0186dcb90814ae9a69b07cedcd944af8
Marovlo <352907423@qq.com>
2023-04-14 17:21:39 +0000

    modi

--- onlyCM-Client.py
+++ onlyCM-Client.py
@@ -11,15 +11,14 @@ data=tool.rand_tensor_byte((3,3))
 conn.handshake(data_size=len(data))
 conn.send(data)
 
-# # 验证write，使用同步write即sync_write_send
-# data_size=len(data)
-# conn.handshake(data_size=data_size) # 告知对端即将写入的大小
-# remote_info=conn.handshake() # 等待对端告知内存的地址和key
-# mr = conn.reg_write(data_size)
-# conn.cmid.post_write(mr, data_size, remote_info['remote_addr'], remote_info['remote_key'])
-# conn.handshake() # 告知对端写入完成、
+# 验证write，使用同步write即sync_write_send
+data=tool.rand_tensor_byte((3,3))
+data_size=len(data)
+conn.handshake(data_size=data_size) # 告知对端即将写入的大小
+remote_info=conn.handshake() # 等待对端告知内存的地址和key
+conn.cmid.write(data, remote_info['remote_addr'], remote_info['remote_key'])
+conn.handshake() # 告知对端写入完成、
 
 # 验证对端read
 data=tool.rand_tensor_byte((3,3))
 conn.sync_read_send(data)
-
--- onlyCM-Server.py
+++ onlyCM-Server.py
@@ -11,12 +11,12 @@ data_size=conn.handshake()['data_size']
 tensor=tool.byte_to_tensor(conn.recv(data_size))
 print(tensor)
 
-# # 验证对端write，即本端建立mr供对端write
-# data_size = conn.handshake()['data_size']  # 获取对端要写入的大小
-# mr = conn.reg_write(size=data_size)  # 注册对应大小的内存
-# conn.handshake(remote_addr=mr.buf, remote_key=mr.rkey)  # 告知对端内存的地址和key
-# conn.handshake()  # 等待对端告知写入完成
-# print(mr.read(data_size, 0))
+# 验证对端write，即本端建立mr供对端write
+data_size = conn.handshake()['data_size']  # 获取对端要写入的大小
+mr = conn.reg_write(size=data_size)  # 注册对应大小的内存
+conn.handshake(remote_addr=mr.buf, remote_key=mr.rkey)  # 告知对端内存的地址和key
+conn.handshake()  # 等待对端告知写入完成
+print(mr.read(data_size, 0))
 
 # 验证read对端，即本端read对端已建立的内存
 data=conn.sync_read_send()
