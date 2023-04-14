51a6b8e73f6ea67dd16e9fce64731651d77b202d
Marovlo <352907423@qq.com>
2023-04-14 16:50:55 +0000

    modi

--- onlyCM-Client.py
+++ onlyCM-Client.py
@@ -11,11 +11,16 @@ data=tool.rand_tensor_byte((3,3))
 conn.handshake(data_size=len(data))
 conn.send(data)
 
-# 验证write，使用同步write即sync_write_send
-data_size=len(data)
-conn.handshake(data_size=data_size) # 告知对端即将写入的大小
-remote_info=conn.handshake() # 等待对端告知内存的地址和key
-mr = conn.reg_write(data_size)
-conn.cmid.post_write(mr, data_size, remote_info['remote_addr'], remote_info['remote_key'])
-conn.handshake() # 告知对端写入完成
+# # 验证write，使用同步write即sync_write_send
+# data_size=len(data)
+# conn.handshake(data_size=data_size) # 告知对端即将写入的大小
+# remote_info=conn.handshake() # 等待对端告知内存的地址和key
+# mr = conn.reg_write(data_size)
+# conn.cmid.post_write(mr, data_size, remote_info['remote_addr'], remote_info['remote_key'])
+# conn.handshake() # 告知对端写入完成、
 
+# 验证对端read
+data=tool.rand_tensor_byte(())
+data_size=len(data)
+mr=conn.reg_read(data_size)
+mr.write(data,data_size)
--- onlyCM-Server.py
+++ onlyCM-Server.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3.8
-
 from easyRDMACM import easyRDMACM
 from tensorTools import TENSORTOOLS
 conn=easyRDMACM()
@@ -12,9 +11,14 @@ data_size=conn.handshake()['data_size']
 tensor=tool.byte_to_tensor(conn.recv(data_size))
 print(tensor)
 
-# 验证对端write，即本端建立mr供对端write
-data_size = conn.handshake()['data_size']  # 获取对端要写入的大小
-mr = conn.reg_write(size=data_size)  # 注册对应大小的内存
-conn.handshake(remote_addr=mr.buf, remote_key=mr.rkey)  # 告知对端内存的地址和key
-conn.handshake()  # 等待对端告知写入完成
-print(mr.read(data_size, 0))
+# # 验证对端write，即本端建立mr供对端write
+# data_size = conn.handshake()['data_size']  # 获取对端要写入的大小
+# mr = conn.reg_write(size=data_size)  # 注册对应大小的内存
+# conn.handshake(remote_addr=mr.buf, remote_key=mr.rkey)  # 告知对端内存的地址和key
+# conn.handshake()  # 等待对端告知写入完成
+# print(mr.read(data_size, 0))
+
+# 验证read对端，即本端read对端已建立的内存
+remote_info=conn.handshake()
+mr=conn.reg_read(remote_info['data_size'])
+tensor=conn.read(remote_addr=remote_info['remote_addr'],remote_key=remote_info['remote_key'])
