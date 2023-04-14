40f5476265954b867266e724e0315920a6734441
Marovlo <352907423@qq.com>
2023-04-14 15:57:33 +0000

    modi

--- onlyCM-Server.py
+++ onlyCM-Server.py
@@ -12,14 +12,6 @@ data_size=conn.handshake()['data_size']
 tensor=tool.byte_to_tensor(conn.recv(data_size))
 print(tensor)
 
-# 验证对端write，即本端建立mr供对端write
-data_size=conn.handshake()['data_size'] # 获取对端要写入的大小
-mr=conn.reg_mr(size=data_size) # 注册对应大小的内存
-conn.handshake(remote_addr=mr.buf,remote_key=mr.rkey) # 告知对端内存的地址和key
-conn.handshake() # 等待对端告知写入完成
-tensor=tool.byte_to_tensor(mr.read(data_size,0))
+# 验证对端write，使用同步write即sync_write_recv
+tensor=tool.byte_to_tensor(conn.sync_write_recv())
 print(tensor)
-
-# 验证本端read，即对端建立mr供本端read
-data_size=conn.handshake()['data_size']
-
