15f0dcbf7ce2c9ab8c15fec21b6238bb7cca4bee
Marovlo <352907423@qq.com>
2023-04-14 17:40:49 +0000

    modi

--- onlyCM-Server.py
+++ onlyCM-Server.py
@@ -16,8 +16,8 @@ data_size = conn.handshake()['data_size']  # 获取对端要写入的大小
 mr = conn.reg_write(size=data_size)  # 注册对应大小的内存
 conn.handshake(remote_addr=mr.buf, remote_key=mr.rkey)  # 告知对端内存的地址和key
 print(mr.buf,mr.rkey)
-# conn.handshake()  # 等待对端告知写入完成
-# print(mr.read(data_size, 0))
+conn.handshake()  # 等待对端告知写入完成
+print(mr.read(data_size, 0))
 
 # 验证read对端，即本端read对端已建立的内存
 data=conn.sync_read_send()
