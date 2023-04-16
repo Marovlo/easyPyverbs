ba6297c81e99eb8b789e5ce81700df73b617e9be
Marovlo <352907423@qq.com>
2023-04-16 09:44:43 +0000

    modi

--- onlyCM-Client.py
+++ onlyCM-Client.py
@@ -2,6 +2,9 @@
 
 from easyRDMACM import easyRDMACM
 from tensorTools import TENSORTOOLS
+import time
+
+start=time.time()
 info_conn=easyRDMACM()
 info_conn.connect(dst_ip='192.168.1.10',dst_port=12345)
 data_conn=easyRDMACM()
@@ -25,17 +28,5 @@ mr.write(data,data_size)
 info_conn.send_infos(data_size=data_size,addr=mr.buf,rkey=mr.rkey)
 read_finished=info_conn.recv_infos()['read_finished']
 
-data_size=info_conn.recv_infos()['data_size']
-mr=data_conn.reg_write(data_size)
-info_conn.send_infos(addr=mr.buf,rkey=mr.rkey)
-send_finished=info_conn.recv_infos()['send_finished']
-data=mr.read(data_size,0)
-print(len(data))
-
-remote_info=info_conn.recv_infos()
-data_size=remote_info['data_size']
-mr=data_conn.reg_read(data_size)
-data_conn.post_read(mr,data_size,remote_info['addr'],remote_info['rkey'])
-info_conn.send_infos(read_finished=1)
-data=mr.read(data_size,0)
-print(len(data))
+end=time.time()
+print("time used:",end-start)
--- onlyCM-Server.py
+++ onlyCM-Server.py
@@ -1,8 +1,12 @@
 #!/usr/bin/python3.8
 from easyRDMACM import easyRDMACM
 from tensorTools import TENSORTOOLS
+import socket
+import time
 
 # 测试了好几天，感觉必须最好得分为数据通路和控制通路，不然会出各种问题，除非只用post_send和post_recv就可以只需要一个通路
+
+start=time.time()
 info_conn=easyRDMACM()
 info_conn.listen(src_ip='192.168.1.10',src_port=12345)
 data_conn=easyRDMACM()
@@ -24,22 +28,12 @@ info_conn.send_infos(read_finished=1)
 data=mr.read(data_size,0)
 print(len(data))
 
-data=tool.rand_tensor_byte((10,10,10,10,10,10))
-data_size=len(data)
-mr=data_conn.reg_write(data_size)
-mr.write(data,data_size)
-info_conn.send_infos(data_size=data_size)
-remote_info=info_conn.recv_infos()
-data_conn.post_write(mr,data_size,remote_info['addr'],remote_info['rkey'])
-wc=data_conn.get_send_comp()
-info_conn.send_infos(send_finished=1)
+end=time.time()
+print("time used:",end-start)
 
-data=tool.rand_tensor_byte((10,10,10,10,10,10))
-data_size=len(data)
-mr=data_conn.reg_read(data_size)
-mr.write(data,data_size)
-info_conn.send_infos(data_size=data_size,addr=mr.buf,rkey=mr.rkey)
-read_finished=info_conn.recv_infos()['read_finished']
+
+
+
 
 
 
