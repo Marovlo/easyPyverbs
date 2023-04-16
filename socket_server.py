970e9a41280870de09bd00f26c62e65f3f6d5145
Marovlo <352907423@qq.com>
2023-04-16 11:20:24 +0000

    modi

--- socket_server.py
+++ socket_server.py
@@ -10,8 +10,13 @@ skt=socket.socket(family=socket.AF_INET,type=socket.SOCK_STREAM)
 skt.bind(('10.112.241.161',12344))
 skt.listen()
 conn,addr=skt.accept()
-data_size=struct.unpack('i',conn.recv(struct.calcsize("i")))
+data_size=struct.unpack('i',conn.recv(struct.calcsize("i")))[0]
 print(data_size)
+recv_size=0
+data=b''
+while recv_size<data_size:
+    data+=conn.recv(1024)
+    recv_size+=1024
 # data=conn.recv(400000811)
 # print(len(data))
 end=time.time()
