dfbda6530171f8bcb31c458bffd4c03747bbb5df
Marovlo <352907423@qq.com>
2023-04-16 08:41:47 +0000

    modi

--- onlyCM-Server.py
+++ onlyCM-Server.py
@@ -7,6 +7,12 @@ data_conn=easyRDMACM()
 data_conn.listen(src_ip='192.168.1.10',src_port=12346)
 tool=TENSORTOOLS()
 
+data_size=info_conn.recv_infos()['data_size']
+mr=data_conn.cmid.reg_msgs(data_size)
+info_conn.send_infos(addr=mr.buf,rkey=mr.rkey)
+send_finished=info_conn.recv_infos()['send_finished']
+data=mr.read(data_size,0)
+print(tool.byte_to_tensor(data))
 
 
 
