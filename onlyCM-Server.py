3da7e93c202d251aa60c6e8fd8c54421c92126e4
Marovlo <352907423@qq.com>
2023-04-16 08:52:34 +0000

    modi

--- onlyCM-Server.py
+++ onlyCM-Server.py
@@ -8,7 +8,7 @@ data_conn.listen(src_ip='192.168.1.10',src_port=12346)
 tool=TENSORTOOLS()
 
 data_size=info_conn.recv_infos()['data_size']
-mr=data_conn.cmid.reg_write(data_size)
+mr=data_conn.reg_write(data_size)
 info_conn.send_infos(addr=mr.buf,rkey=mr.rkey)
 send_finished=info_conn.recv_infos()['send_finished']
 data=mr.read(data_size,0)
@@ -16,8 +16,8 @@ print(tool.byte_to_tensor(data))
 
 remote_info=info_conn.recv_infos()
 data_size=remote_info['data_size']
-mr=data_conn.cmid.reg_read(data_size)
-data_conn.cmid.post_read(mr,data_size,remote_info['addr'],remote_info['rkey'])
+mr=data_conn.reg_read(data_size)
+data_conn.post_read(mr,data_size,remote_info['addr'],remote_info['rkey'])
 info_conn.send_infos(read_finished=1)
 data=mr.read(data_size,0)
 print(tool.byte_to_tensor(data))
